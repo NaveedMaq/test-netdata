4df5f0c2f26a63bd439836f16f4157313120094b
Naveed <45787041+NaveedMaq@users.noreply.github.com>
2025-04-15 16:45:04 +0530

    feat(add): test endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,74 @@ import dotenv from 'dotenv';
 import { todoRouter } from './routes/todo.routes';
 dotenv.config();
 
+import { StatsD } from 'hot-shots';
+
+const statsd = new StatsD({
+  host: '127.0.0.1',
+  port: 8125,
+  prefix: 'express.',
+});
+
 const app = express();
 
 app.use(express.json());
 
 const port = process.env.PORT;
 
+app.use((req, res, next) => {
+  const start = process.hrtime();
+
+  res.on('finish', () => {
+    const [seconds, nanoseconds] = process.hrtime(start);
+    const durationMs = seconds * 1e3 + nanoseconds / 1e6;
+
+    const route = req.route?.path || req.path;
+    const method = req.method.toLowerCase();
+    const status = res.statusCode;
+
+    // Example metric: express.api_get_users.response_time
+    statsd.timing(
+      `api_${method}_${route.replace(/\//g, '_')}.response_time`,
+      durationMs
+    );
+    statsd.increment(
+      `api_${method}_${route.replace(/\//g, '_')}.status_code.${status}`
+    );
+  });
+
+  next();
+});
+
+app.get('/hold/:time', async (req, res) => {
+  const time = parseInt(req.params.time, 10);
+
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(null);
+    }, time);
+  });
+
+  res.json({
+    timeABC: time,
+  });
+});
+
+app.post('/stop/:time', async (req, res) => {
+  const time = parseInt(req.params.time, 10);
+
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(null);
+    }, time);
+  });
+
+  res.json({
+    timeABC: time,
+  });
+});
+
 app.use('/todos', todoRouter);
 
 app.listen(port, () => {
-    console.log('Server successfully started on port', port);
+  console.log('Server successfully started on port', port);
 });
--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,50 +1,50 @@
 import crypto from 'crypto';
 
 export type ITodo = {
-    id: string;
-    title: string;
-    description: string;
+  id: string;
+  title: string;
+  description: string;
 };
 
 class TodoModel {
-    private todos: ITodo[];
+  private todos: ITodo[];
 
-    constructor() {
-        this.todos = [];
-    }
+  constructor() {
+    this.todos = [];
+  }
 
-    getAll(): ITodo[] {
-        return [...this.todos];
-    }
+  getAll(): ITodo[] {
+    return [...this.todos];
+  }
 
-    getById(id: string): ITodo | null {
-        return this.todos.find((todo) => todo.id === id) ?? null;
-    }
+  getById(id: string): ITodo | null {
+    return this.todos.find((todo) => todo.id === id) ?? null;
+  }
 
-    create(todo: Omit<ITodo, 'id'>) {
-        const newTodo = {
-            id: this.generateNewId(4),
-            title: todo.title,
-            description: todo.description,
-        };
-        this.todos.push(newTodo);
+  create(todo: Omit<ITodo, 'id'>) {
+    const newTodo = {
+      id: this.generateNewId(4),
+      title: todo.title,
+      description: todo.description,
+    };
+    this.todos.push(newTodo);
 
-        return newTodo;
-    }
+    return newTodo;
+  }
 
-    deleteById(id: string) {
-        this.todos = this.todos.filter((todo) => todo.id !== id);
-    }
+  deleteById(id: string) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
 
-    private generateNewId(length: number) {
-        let newId: string;
+  private generateNewId(length: number) {
+    let newId: string;
 
-        do {
-            newId = crypto.randomBytes(length).toString('hex');
-        } while (this.getById(newId));
+    do {
+      newId = crypto.randomBytes(length).toString('hex');
+    } while (this.getById(newId));
 
-        return newId;
-    }
+    return newId;
+  }
 }
 
 export const todoModel = new TodoModel();
